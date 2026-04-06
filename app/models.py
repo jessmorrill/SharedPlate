044f9e54bed0d75f75d41b7f1ba682f89c7f942e
Jessica Morrill <jessm3904@gmail.com>
2026-04-06 19:57:29 +0000

    added template and route examples to build on. expanded database to allow for group membership and ingreedients.

--- app/models.py
+++ app/models.py
@@ -1,19 +1,12 @@
 from app import db
 
 # Define the DB schema
-class City(db.Model):
-    __tablename__ = 'cities'
-    id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(64), unique=True, nullable=False)
-    population = db.Column(db.Integer, unique=False, nullable=False)
-
 class Recipe(db.Model):
     __tablename__ = 'recipe'
     id = db.Column(db.Integer, primary_key=True)
     user_email = db.Column(db.String(64), db.ForeignKey('user.email'), nullable=False)
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False, )
     title = db.Column(db.String(64), nullable=False)
-    allergens = db.Column(db.array, nullable=True)
     prep_time = db.Column(db.Integer, nullable = False)
     cook_time = db.Column(db.Integer, nullable=False)
     body = db.Column(db.TEXT, nullable=False)
@@ -21,13 +14,35 @@ class Recipe(db.Model):
     num_serves = db.Column(db.Integer, nullable = False)
     privacy_setting = db.Column(db.String(64), nullable=False)
     is_validated = db.Column(db.Boolean, nullable=True)
-    date_posted = db.Column(db.datetime, nullable=False)
+    date_posted = db.Column(db.TEXT, nullable=False)
     forked_from_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=True)
 
 class User(db.Model):
     __tablename__ = 'user'
-    email = db.Column(db.String(64), primary_key=True, nullable=False)
+    email = db.Column(db.String(64), primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable = False)
-    date_joined = db.Column(db.datetime, nullable=False)
+    date_joined = db.Column(db.TEXT, nullable=False)
     isPending = db.Column(db.Boolean, nullable=False)
+
+class Group(db.Model):
+    __tablename__ = 'group'
+    id = db.Column(db.Integer, primary_key=True)
+    group_name = db.Column(db.String(64), nullable=False)
+    privacy_setting = db.Column(db.String(64), nullable=False)
+
+class Group_Membership(db.Model):
+    __tablename__ = 'group_membership'
+    user_email = db.Column(db.String(64), db.ForeignKey('user.email'), primary_key=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), primary_key=True)
+    role = db.Column(db.String(64), nullable=False)
+    notify_if_review = db.Column(db.Boolean, nullable=False)
+    notify_if_fork = db.Column(db.Boolean, nullable=False)
+    notify_if_change = db.Column(db.Boolean, nullable=False)
+
+class Ingredient(db.Model):
+    __tablename__ = 'ingredient'
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), primary_key=True)
+    name = db.Column(db.String(64), primary_key=True)
+    num = db.Column(db.Integer, nullable=False)
+    units = db.Column(db.String(64), nullable=False)
