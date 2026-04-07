3646f55576cbee8c1bef19bcdea4fdfdb4789975
Jessica Morrill <jessm3904@gmail.com>
2026-04-07 13:26:09 +0000

    Added Form Validators

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, RadioField, TextAreaField, FieldList, FormField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, NumberRange
 from wtforms import Form
 
 class IngredientForm(Form):
-    num= IntegerField("Quantity:", validators=[Optional()])
+    num= IntegerField("Quantity:", validators=[Optional(), NumberRange(min=1, message="Must be at least 1")])
     ingredient_name = StringField("Ingredient:", validators=[Optional()])
     unit = StringField("Unit:", validators=[Optional()])
 
 class CreateRecipe(FlaskForm):
     title = StringField("Recipe Title:",validators=[DataRequired()])
-    prep_time = IntegerField("Prep Time (minutes):",validators=[DataRequired()])
-    cook_time = IntegerField("Cook Time (minutes):",validators=[DataRequired()])
+    prep_time = IntegerField("Prep Time (minutes):",validators=[DataRequired(), NumberRange(min=0, message="Cannot be negative")])
+    cook_time = IntegerField("Cook Time (minutes):",validators=[DataRequired(), NumberRange(min=0, message="Cannot be negative")])
     body = TextAreaField("Instructions:",validators=[DataRequired()])
-    num_serves = IntegerField("Number of Servings:",validators=[DataRequired()])
+    num_serves = IntegerField("Number of Servings:",validators=[DataRequired(), NumberRange(min=1, message="Must be at least 1")])
     ingredients = FieldList(FormField(IngredientForm), min_entries=1)
     privacy = RadioField("Privacy Settings:", choices = [("public", "Visible to Everyone"), ("private", "Visible to Group Only"), ("unlisted", "Visible to Me Only")],validators=[DataRequired()])
     category = RadioField("Category:", choices = [("appetizer", "Appetizer"), ("breakfast", "Breakfast"), ("lunch", "Lunch"), ("dinner", "Dinner"), ("dessert", "Dessert")],validators=[DataRequired()])
